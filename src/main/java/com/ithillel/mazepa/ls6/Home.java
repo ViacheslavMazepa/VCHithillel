888a8fc5c89443698e80cf0a4700e866a1af3d9d
viacheslavmazepa <viacheslav.mazepa@loopme.com>
2023-03-07 20:12:48 +0200

    hw6

--- src/main/java/com/ithillel/mazepa/ls6/Home.java
+++ src/main/java/com/ithillel/mazepa/ls6/Home.java
@@ -3,17 +3,17 @@ package com.ithillel.mazepa.ls6;
 public class Home {
     public static void main(String[] args) {
 
-        int counter = 1;
+        int counter = 0;
+        int id = 0;
 
-        for ( ; counter < 100; counter++) {
-            if (counter % 10 == 4 || counter % 10 == 9 || counter / 10 == 4 || counter / 10 == 9) {
+        for ( int i = 0; counter < 100; i++) {
+            id++;
+            if (id % 10 == 4 || id % 10 == 9 || id / 10 == 4 || id / 10 == 9 || id / 10 == 14 || id / 10 == 19 ) {
                 continue;
             }
-            System.out.println("shuttle number: " + counter);
             counter++;
+            System.out.println("shuttle number: " + id);
         }
-
-        System.out.println();
         System.out.println("total numbers = " + counter);
     }
 }

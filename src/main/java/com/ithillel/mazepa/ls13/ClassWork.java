5b98dcea2d022a6196968f08a843dd85e63b73fb
Pascal PC <geiger.stelus@gmail.com>
2023-04-08 18:57:43 +0300

    update

---
+++ src/main/java/com/ithillel/mazepa/ls13/ClassWork.java
@@ -0,0 +1,9 @@
+package com.ithillel.mazepa.ls13;
+
+public class ClassWork {
+    //пм + дев + куа чи занйшов помилку в РЦ
+    //вийшов у вікно + згорів на роботі + задоволений собою 
+    ClassWork(){
+
+    }
+}
---
+++ src/main/java/com/ithillel/mazepa/ls13/ClassWorkT.java
@@ -0,0 +1,7 @@
+package com.ithillel.mazepa.ls13;
+
+public class ClassWorkT {
+    ClassWorkT(boolean isBrain){
+        System.out.println("have brain" +isBrain);
+    }
+}

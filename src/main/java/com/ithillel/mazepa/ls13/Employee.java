4c62a6ecd5b5d4abac212508a3687d99f9e19873
viacheslavmazepa <viacheslav.mazepa@loopme.com>
2023-04-09 13:39:18 +0300

    HW 9. OOP first part

---
+++ src/main/java/com/ithillel/mazepa/ls13/Employee.java
@@ -0,0 +1,23 @@
+package com.ithillel.mazepa.ls13;
+
+class Employee {
+    protected String name;
+    public int age = 0;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/ithillel/mazepa/ls13/Manager.java
@@ -0,0 +1,5 @@
+package com.ithillel.mazepa.ls13;
+
+public class Manager {
+    int numberOfSubordinates = 0;
+}
